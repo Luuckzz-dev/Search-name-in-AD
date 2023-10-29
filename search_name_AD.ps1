fddf3dfc06a0feb2f66cb0be987caa99cbdde08d
Lucas Gonzales <63990673+Luuckzz-dev@users.noreply.github.com>
2023-10-29 15:28:34 -0300

    Add files via upload

---
+++ search_name_AD.ps1
@@ -0,0 +1,28 @@
+﻿# Caminho para o arquivo Excel
+$excelFilePath = "C:\Users\lgonzales\Desktop\shellscript\carlos\app\ad.xlsx"
+
+# Nome da coluna que contém os nomes dos colaboradores
+$columnName = "Nome do Colaborador"
+
+# Carregar o módulo do Active Directory
+Import-Module ActiveDirectory
+
+# Ler o arquivo Excel e extrair os nomes dos colaboradores
+$excelData = Import-Excel -Path $excelFilePath
+$colaboradores = $excelData | Select-Object -ExpandProperty $columnName
+
+# Iterar pelos nomes dos colaboradores e buscar no Active Directory
+foreach ($nome in $colaboradores) {
+    $user = Get-ADUser -Filter {DisplayName -eq $nome}
+
+    if ($user) {
+        Write-Host "Nome do Colaborador: $nome"
+        Write-Host "Usuário encontrado no AD: $($user.SamAccountName)"
+        Write-Host "-----------------------------"
+    } else {
+        Write-Host "Nome do Colaborador: $nome"
+        Write-Host "Usuário não encontrado no AD"
+        Write-Host "-----------------------------"
+    }
+}
+
